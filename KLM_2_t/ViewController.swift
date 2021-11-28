4c2a33efd0b85156bc4746fb157f3d0e92ab1098
Pavle Mijatovic <paul.mijatovic@sprintingsoftware.com>
2021-11-28 19:57:08 +0100

    Initial project setup

---
+++ KLM_2_t/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  KLM_2_t
+//
+//  Created by Pavle Mijatovic on 27.11.21..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
